a86c64caed5928e0af084a0d0d89235945eb12f4
William Cannon <william.cannon@gmail.com>
2023-02-16 13:41:47 -0600

    this works

--- main.tf
+++ main.tf
@@ -1,59 +1,77 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+data "archive_file" "lambda_zip" {
+  type             = "zip"
+  source_file      = "schedule_test.py"
+  output_file_mode = "0666"
+  output_path      = "schedule_test.py.zip"
+}
+
+resource "aws_lambda_function" "processing_lambda" {
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = "testwww"
+  handler          = "schedule_test.lambda_handler"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  role             = aws_iam_role.processing_lambda_role.arn
+
+  runtime = "python3.9"
+  timeout = 900
+  #memory_size = 10240
+  #tags        = var.config.tags
+}
+
+resource "aws_iam_role" "processing_lambda_role" {
+  name = "test-www-role"
+  path = "/service-role/"
+  #permissions_boundary = local.config.permission_boundary_arn
+  assume_role_policy = data.aws_iam_policy_document.assume-role-policy_document.json
+
+  inline_policy {
+    name   = "test_policy"
+    policy = data.aws_iam_policy_document.test_policy_document.json
+  }
+}
+
+data "aws_iam_policy_document" "assume-role-policy_document" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
     }
   }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "us-west-2"
-}
-
-resource "aws_iam_role" "scheduler_role" {
-  name = "scheduler_role"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Resource": "arn:aws:lambda:us-east-1:012800249358:function:example-lambda",
-          "Principal": {
-            "Service": "events.amazonaws.com"
-          }
-          "Effect" : "Allow",
-          "Action" : [
-            "lambda:InvokeFunction"
-          ],
-        }
-      ]
-    }
-  )
-
-  tags = {
-    tag-key = "tag-value"
-  }
 }
 
 
-resource "aws_scheduler_schedule" "example" {
-  name       = "my-lambda-schedule"
-  group_name = "default"
+data "aws_iam_policy_document" "test_policy_document" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:AssociateKmsKey"
+    ]
+    resources = ["*"]
 
-  flexible_time_window {
-    mode = "OFF"
   }
+}
 
-  schedule_expression = "rate(1 hour)"
+resource "aws_cloudwatch_event_rule" "schedule" {
+  name        = "schedule"
+  description = "Schedule for Lambda Function"
+  #schedule_expression = rate(1 minutes)
+  schedule_expression = "cron(0/10 * ? * MON-FRI *)"
+}
 
-  target {
-    arn      = "arn:aws:lambda:us-east-1:012800249358:function:example-lambda"
-    role_arn = aws_iam_role.scheduler_role.arn
-  }
+resource "aws_cloudwatch_event_target" "schedule_lambda" {
+  rule      = aws_cloudwatch_event_rule.schedule.name
+  target_id = "processing_lambda"
+  arn       = aws_lambda_function.processing_lambda.arn
+}
+
+
+resource "aws_lambda_permission" "allow_events_bridge_to_run_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.processing_lambda.function_name
+  principal     = "events.amazonaws.com"
 }
--- variables.tf
+++
@@ -1,6 +0,0 @@
-variable "rate_in_minutes" {
-  description = "Value of Cloudwatch Eventbridge schedule in minutes"
-  type        = number
-  default     = 720 # every 12 hours
-}
-
